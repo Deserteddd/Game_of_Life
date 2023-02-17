bd09e19b55c90c1a5d30f51b30e5adacbb632b5a
Deserteddd <eelis.autio98@gmail.com>
2023-02-17 04:23:01 +0200

    Added pattern recognition

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,81 @@
 use core::ops::Range;
+use std::{thread, time, collections::HashMap};
 
 const GRIDLEN: usize = 40;
 const MAX_INDEX: usize = GRIDLEN-1;
 
+#[derive(Hash, Eq, PartialEq)]
 pub struct Board{
     grid: [[bool; GRIDLEN]; GRIDLEN],
     swaps: Vec<(usize, usize)>
+}
+
+pub struct Game{
+    board: Board,
+    past_states: HashMap<[[bool; GRIDLEN]; GRIDLEN], u64>,
+    sleeptime: time::Duration
+}
+
+impl Game{
+    pub fn run(&mut self){
+        let mut count: u64 = 1;
+        loop{
+            let start = time::Instant::now();
+            self.past_states.insert(self.board.get_state(), count);
+            print!("\x1B[2J\x1B[1;1H");
+            self.tick();
+            if self.past_states.contains_key(&self.board.get_state()){
+                let index = self.past_states.get(&self.board.get_state()).unwrap();
+                self.print_results(index, &count);
+                break;
+            } else {
+                count += 1;
+            }
+            // println!("Frame time: {:?}", start.elapsed());
+            // thread::sleep(time::Duration::from_millis(50));
+        }
+    }
+
+    fn print_results(&self, index: &u64, count: &u64){
+        let pattern_length = count-index;
+        match pattern_length{
+            1 => println!("Pattern became static at iteration: {index}"),
+            _ => println!("repeating pattern between iterations {} and {}
+                          \nLooping pattern length: {}",index, count, pattern_length),
+        }
+
+
+    }
+
+    pub fn set_sleeptime(&mut self, n: u64){
+        self.sleeptime = time::Duration::from_millis(n);
+    }
+
+    fn tick(&mut self){
+        self.board.update();
+        self.board.draw();
+    }
+}
+
+impl From<Board> for Game{
+    fn from(board: Board) -> Self{
+        Game { 
+            board: board,    
+            past_states: HashMap::new(),
+            sleeptime: time::Duration::from_millis(10),
+        }
+    }
 }
 
 impl From<Vec<(usize, usize)>> for Board{
     fn from(coordinates: Vec<(usize, usize)>) -> Self{
         let mut board = Board{grid: [[false; GRIDLEN]; GRIDLEN],
                               swaps: Vec::new()};
-
         for i in coordinates{
             if i.0 < GRIDLEN && i.1 < GRIDLEN{
                 board.grid[i.0][i.1] = true;
             }
         }
-
         board
     }
 }
@@ -57,9 +114,14 @@ impl Board{
 
             } 
         }
+
         for i in self.swaps.iter(){
             self.grid[i.0][i.1] ^= true; 
         }
+    }
+
+    pub fn get_state(&self) -> [[bool; GRIDLEN]; GRIDLEN]{
+        self.grid
     }
 
     fn live_neighbours(&self, x: usize, y: usize) -> u8{
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,16 @@
 use gameoflife::*;
-use std::{thread, time};
+
 
 fn main() {
     let gun: Vec<(usize, usize)> = vec![
     (13, 2),  (14, 2),  (13, 3),  (14, 3),  (13, 12), (14, 12), (15, 12), (12, 37),
     (12, 13), (16, 13), (11, 14), (17, 14), (11, 15), (17, 15), (14, 16), (12, 17),
-    (16, 17), (13, 18), (14, 18), (15, 18), (14, 19), (11, 22), (12, 22), (13, 22), 
+    (16, 17), (13, 18), (14, 18), (15, 18), (14, 19), (11, 2), (12, 22), (13, 22), 
     (11, 23), (12, 23), (13, 23), (10, 24), (14, 24), (9, 26),  (10, 26), (14, 26), 
     (15, 26), (11, 36), (12, 36), (11, 37)];
-
-    game_loop(Board::from(gun));
-}
-
-fn game_loop(mut board: Board){
-    loop{
-        let start = time::Instant::now();
-        print!("\x1B[2J\x1B[1;1H");
-        board.draw();
-        board.update();
-        println!("Frame time: {:?}", start.elapsed());
-        thread::sleep(time::Duration::from_millis(100));
-    }
+    let gun_board = Board::from(gun);
+    let mut game = Game::from(gun_board);
+    game.run()
 }
 
 

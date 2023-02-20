622d88a281f2ee1e21911eb1499ccba677ab22ba
Deserteddd <eelis.autio98@gmail.com>
2023-02-20 05:54:58 +0200

    added Config

--- src/lib.rs
+++ src/lib.rs
@@ -1,59 +1,114 @@
 use core::ops::Range;
-use std::{thread, time, collections::HashMap};
+use std::{thread, time::{self, Duration}, collections::HashMap};
 
 const GRIDLEN: usize = 40;
 const MAX_INDEX: usize = GRIDLEN-1;
 
-#[derive(Hash, Eq, PartialEq)]
-pub struct Board{
-    grid: [[bool; GRIDLEN]; GRIDLEN],
-    swaps: Vec<(usize, usize)>
+// Trait Definitions //
+pub trait Draw {
+    fn draw(&self);
 }
 
+pub trait Configure {
+    fn configure(&mut self, c: Config);
+}
+
+
+// Config 
+pub struct Config{
+    pub args: Vec<String>,
+    pub draws: bool,
+    pub returns: bool,
+    pub sleeptime: u64
+}
+
+
+// Results
+pub struct Results{
+    final_board: Board,
+    final_count: u64,
+    repeating_key: u64,
+}
+
+impl Results{
+    pub fn to_board(&self) -> Board{
+        Board::from(self.final_board.get_state())
+    }
+}
+
+impl Draw for Results{
+    fn draw(&self){
+        // index == self.repeating_key, count == self.final_count
+
+        self.final_board.draw();
+        let pattern_length = self.final_count - self.repeating_key;
+        
+        match pattern_length{ 
+            0 => println!("Pattern became static at iteration: {}", self.repeating_key),
+
+            1 => println!("Alternating pattern at iteration: {}", self.repeating_key),
+
+            _ => println!("Repeating pattern between iterations {} and {}
+                          \nLooping pattern length: {}",
+                          self.repeating_key, self.final_count, pattern_length),
+        }
+    }
+}
+
+
+// Game
 pub struct Game{
     board: Board,
-    past_states: HashMap<[[bool; GRIDLEN]; GRIDLEN], u64>,
-    sleeptime: time::Duration
+    past_states: HashMap<Vec<Vec<bool>>, u64>,
+    sleeptime: time::Duration,
+    draw: bool,
+    returns: bool
 }
 
 impl Game{
-    pub fn run(&mut self){
-        let mut count: u64 = 1;
+    pub fn run(&mut self) -> Option<Results>{
+        let mut count: u64 = 0;        
         loop{
-            let start = time::Instant::now();
+            count += 1;
             self.past_states.insert(self.board.get_state(), count);
-            print!("\x1B[2J\x1B[1;1H");
             self.tick();
-            if self.past_states.contains_key(&self.board.get_state()){
-                let index = self.past_states.get(&self.board.get_state()).unwrap();
-                self.print_results(index, &count);
-                break;
-            } else {
-                count += 1;
+            let new_state = self.board.get_state();
+            if self.past_states.contains_key(&new_state){
+                match self.returns{
+                    true => return Some(
+                        Results { 
+                            final_board: (self.board.clone()),
+                            final_count: (count),
+                            repeating_key: *self.past_states.get(&new_state).unwrap()
+                        }),
+
+                    false => {
+                        return None;
+                    }
+                }
             }
-            // println!("Frame time: {:?}", start.elapsed());
-            // thread::sleep(time::Duration::from_millis(50));
         }
     }
 
-    fn print_results(&self, index: &u64, count: &u64){
-        let pattern_length = count-index;
-        match pattern_length{
-            1 => println!("Pattern became static at iteration: {index}"),
-            _ => println!("repeating pattern between iterations {} and {}
-                          \nLooping pattern length: {}",index, count, pattern_length),
-        }
 
-
+    fn set_sleeptime(&mut self, n: &u64){
+        self.sleeptime = Duration::from_millis(*n);
     }
 
-    pub fn set_sleeptime(&mut self, n: u64){
-        self.sleeptime = time::Duration::from_millis(n);
+    fn set_draw(&mut self, b: &bool){
+        self.draw = *b;
+    }
+
+    fn set_returns(&mut self, b: &bool){
+        self.returns = *b;
     }
 
     fn tick(&mut self){
+        if self.draw{
+            self.board.draw();
+            thread::sleep(self.sleeptime);
+        }
         self.board.update();
-        self.board.draw();
     }
 }
 
@@ -62,43 +117,42 @@ impl From<Board> for Game{
         Game { 
             board: board,    
             past_states: HashMap::new(),
-            sleeptime: time::Duration::from_millis(10),
+            sleeptime: time::Duration::from_millis(25),
+            draw: true,
+            returns: true,
         }
     }
 }
 
-impl From<Vec<(usize, usize)>> for Board{
+impl From <Vec<(usize, usize)>> for Game {
     fn from(coordinates: Vec<(usize, usize)>) -> Self{
-        let mut board = Board{grid: [[false; GRIDLEN]; GRIDLEN],
-                              swaps: Vec::new()};
-        for i in coordinates{
-            if i.0 < GRIDLEN && i.1 < GRIDLEN{
-                board.grid[i.0][i.1] = true;
-            }
+        Game { 
+            board: Board::from(coordinates),    
+            past_states: HashMap::new(),
+            sleeptime: time::Duration::from_millis(25),
+            draw: true,
+            returns: true,
         }
-        board
     }
+}
+
+impl Configure for Game{
+    fn configure(&mut self, config: Config) {
+        self.set_draw(&config.draws);
+        self.set_sleeptime(&config.sleeptime);
+        self.set_returns(&config.returns);
+    }
+}
+
+
+// Board
+#[derive(Hash, Eq, PartialEq)]
+pub struct Board{
+    grid: Vec<Vec<bool>>,
+    swaps: Vec<(usize, usize)>,
 }
 
 impl Board{
-    pub fn draw(&self){
-        let mut grid = String::new();
-        let floor = String::from_iter(["# "; GRIDLEN+2]);
-        grid.push_str(&(floor.clone() + "\n"));
-
-        for i in self.grid{
-            let line = String::from_iter(i.map(|a| match a{
-                false => "  ",
-                true => "# "
-            }));
-            grid.push_str("# ");
-            grid.push_str(&(line + "#\n"));
-        }
-
-        grid.push_str(&(floor));
-        print!("{}\n", grid);
-    }
- 
     pub fn update(&mut self){
         self.swaps.clear();
         for (x, i) in self.grid.iter().enumerate(){
@@ -120,8 +174,8 @@ impl Board{
         }
     }
 
-    pub fn get_state(&self) -> [[bool; GRIDLEN]; GRIDLEN]{
-        self.grid
+    pub fn get_state(&self) -> Vec<Vec<bool>>{
+        self.grid.clone()
     }
 
     fn live_neighbours(&self, x: usize, y: usize) -> u8{
@@ -153,6 +207,49 @@ impl Board{
     }
 }
 
+impl From<Vec<(usize, usize)>> for Board{
+    fn from(coordinates: Vec<(usize, usize)>) -> Self{
 
+        let mut starting_grid = vec![vec![false; GRIDLEN]; GRIDLEN];
 
+        for i in coordinates{
+            if i.0 < GRIDLEN && i.1 < GRIDLEN{
+                starting_grid[i.0][i.1] = true;
+            }
+        }
 
+        
+        Board{grid: starting_grid, swaps: Vec::new()}
+    }
+}
+
+impl Clone for Board{
+    fn clone(&self) -> Self {
+        Board { grid: self.grid.clone(), swaps: self.swaps.clone() }
+    }
+}
+
+impl From<Vec<Vec<bool>>> for Board{
+    fn from(grid: Vec<Vec<bool>>) -> Self{
+        Board { grid: grid, swaps: Vec::new() }
+    }
+}
+
+impl Draw for Board{
+    fn draw(&self){
+        let mut grid = String::new();
+        let floor = String::from_iter(["# "; GRIDLEN+2]);
+        grid.push_str(&(floor.clone() + "\n"));
+
+        for i in &self.grid{
+            let line = String::from_iter(i.iter().map(|a| match a{
+                false => "  ",
+                true => "# "
+            }));
+            grid.push_str("# ");
+            grid.push_str(&(line + "#\n"));
+        }
+        grid.push_str(&(floor));
+        println!("{esc}[2J{esc}[1;1H{grid}", esc = 27 as char);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 use gameoflife::*;
 
-
 fn main() {
-    let gun: Vec<(usize, usize)> = vec![
-    (13, 2),  (14, 2),  (13, 3),  (14, 3),  (13, 12), (14, 12), (15, 12), (12, 37),
-    (12, 13), (16, 13), (11, 14), (17, 14), (11, 15), (17, 15), (14, 16), (12, 17),
-    (16, 17), (13, 18), (14, 18), (15, 18), (14, 19), (11, 2), (12, 22), (13, 22), 
-    (11, 23), (12, 23), (13, 23), (10, 24), (14, 24), (9, 26),  (10, 26), (14, 26), 
-    (15, 26), (11, 36), (12, 36), (11, 37)];
-    let gun_board = Board::from(gun);
-    let mut game = Game::from(gun_board);
-    game.run()
+    let p1: Vec<(usize, usize)> = vec![
+        (13, 2),  (14, 2),  (13, 3),  (14, 3),  (13, 12), (14, 12), (15, 12), (12, 37),
+        (12, 13), (16, 13), (11, 14), (17, 14), (11, 15), (17, 15), (14, 16), (12, 17),
+        (16, 17), (13, 18), (16, 18), (15, 18), (14, 19), (11, 2), (12, 22), (13, 22), 
+        (11, 23), (12, 23), (13, 23), (10, 24), (14, 24), (9, 26),  (10, 26), (14, 26), 
+        (15, 26), (11, 36), (12, 36), (11, 37)];
+    
+
+    let p1_config = Config{
+        args: Vec::new(),
+        draws: true,
+        returns: true,
+        sleeptime: 40
+    };
+
+    run(p1, p1_config);
 }
 
 
-
+fn run(pattern: Vec<(usize, usize)>, config: Config){
+        let mut game = Game::from(pattern);
+        game.configure(config);
+        let x = game.run();
+        match &x{
+            Some(_) => x.unwrap().draw(),
+            None => println!("Game returned None"),
+        }
+}
